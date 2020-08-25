8a2d9d245311fa18f7e59142c7d961f5a7c1cee2
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2020-08-25 14:39:12 +0200

    add few screens with animated-tabbar that runs on Expo

--- App.tsx
+++ App.tsx
@@ -1,23 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import RootScreen from './screens/Root';
+import BubbleScreen from './screens/Bubble';
+import BubbleStyledScreen from './screens/BubbleStyled';
+import BubbleRTLScreen from './screens/BubbleRTL';
+import BubbleStandaloneScreen from './screens/BubbleStandalone';
+import FlashyScreen from './screens/Flashy';
+import FlashyStyledScreen from './screens/FlashyStyled';
+import FlashyRTLScreen from './screens/FlashyRTL';
+import FlashyStandaloneScreen from './screens/FlashyStandalone';
+import {
+    MaterialIconWithLabelScreen,
+    MaterialIconWithLabelOnFocusScreen,
+    MaterialIconOnlyScreen,
+} from './screens/Material';
+import MaterialStyledScreen from './screens/MaterialStyled';
+import MaterialRTLScreen from './screens/MaterialRTL';
+import MaterialStandaloneScreen from './screens/MaterialStandalone';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+const Stack = createStackNavigator();
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Root" headerMode="none">
+                <Stack.Screen name="Root" component={RootScreen} />
+                <Stack.Screen name="Bubble" component={BubbleScreen} />
+                <Stack.Screen name="BubbleStyled" component={BubbleStyledScreen} />
+                <Stack.Screen name="BubbleRTL" component={BubbleRTLScreen} />
+                <Stack.Screen
+                    name="BubbleStandalone"
+                    component={BubbleStandaloneScreen}
+                />
+                <Stack.Screen name="Flashy" component={FlashyScreen} />
+                <Stack.Screen name="FlashyStyled" component={FlashyStyledScreen} />
+                <Stack.Screen name="FlashyRTL" component={FlashyRTLScreen} />
+                <Stack.Screen
+                    name="FlashyStandalone"
+                    component={FlashyStandaloneScreen}
+                />
+                <Stack.Screen
+                    name="MaterialIconWithLabel"
+                    component={MaterialIconWithLabelScreen}
+                />
+                <Stack.Screen
+                    name="MaterialIconOnly"
+                    component={MaterialIconOnlyScreen}
+                />
+                <Stack.Screen
+                    name="MaterialIconWithLabelOnFocus"
+                    component={MaterialIconWithLabelOnFocusScreen}
+                />
+                <Stack.Screen name="MaterialStyled" component={MaterialStyledScreen} />
+                <Stack.Screen name="MaterialRTL" component={MaterialRTLScreen} />
+                <Stack.Screen
+                    name="MaterialStandalone"
+                    component={MaterialStandaloneScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-  }
 }
